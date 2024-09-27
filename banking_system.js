f3b136895e7c0addbcb18e37ab0f1fe71b63ecc1
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-09-27 22:56:25 +0700

    feat: import bankAccount

---
+++ banking_system.js
@@ -0,0 +1,7 @@
+const BankAccount = require("./bank_account.js");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
