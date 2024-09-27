71470ff833387021fe27473a3df0786336a7b67b
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-09-27 22:36:16 +0700

    feat: withdraw

--- bank_account.js
+++ bank_account.js
@@ -1,8 +1,10 @@
 class BankAccount {
-  constructor(accountName, accountNumber, balance) {
-    this.accountName = accountName;
-    this.accountNumber = accountNumber;
-    this.balance = balance;
+  withdraw(amount) {
+    if (amount > this.balance) {
+      throw new Error("Gagal melakukan penarikan, saldo tidak mencukupi");
+    }
+
+    return (this.balance -= amount);
   }
 }
 
