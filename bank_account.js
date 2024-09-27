9cd891fcc76208c75b92f76fbc822bb280bc54bd
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-09-27 22:47:54 +0700

    fix: withdraw

--- bank_account.js
+++ bank_account.js
@@ -8,7 +8,13 @@ class BankAccount {
   deposit(amount) {
     return (this.balance += amount);
   }
+
+  withdraw(amount) {
+    if (amount > this.balance) {
+      throw new Error("Gagal melakukan penarikan, saldo tidak mencukupi");
+    }
+
+    return (this.balance -= amount);
+  }
 }
-
-
 
